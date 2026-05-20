a6bbc1a83214023d5dd5ce8012fa6dbe57810512
Shivram Sundar <sh191143@ucf.edu>
2026-05-20 11:48:29 -0400

    Add files via upload
    php files for API. (Untested)

---
+++ UpdateContact.php
@@ -0,0 +1,54 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$contactId = $inData["id"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$email = $inData["email"];
+	$phone = $inData["phone"];
+	$address = $inData["address"];
+	$notes = $inData["notes"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Email=?, Phone=?, Address=?, Notes=? WHERE ID=? AND UserID=?");
+		$stmt->bind_param("ssssssss", $firstName, $lastName, $email, $phone, $address, $notes, $contactId, $userId);
+		$stmt->execute();
+		
+		if($stmt->affected_rows > 0)
+		{
+			returnWithError("");
+		}
+		else
+		{
+			returnWithError("Contact not found or no changes were made");
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
