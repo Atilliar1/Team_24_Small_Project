4f9c5a3375aa186e63ee0c32af9832727b1d97ae
Shivram Sundar <sh191143@ucf.edu>
2026-05-21 11:24:00 -0400

    Registering portion of API.

---
+++ Register.php
@@ -0,0 +1,73 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$login = $inData["login"];
+	$password = $inData["password"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$id = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Check if login already exists
+		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $result->num_rows > 0 )
+		{
+			returnWithError("Login already exists");
+		}
+		else
+		{
+			// Insert new user
+			$stmt = $conn->prepare("INSERT INTO Users (Login, Password, firstName, lastName) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $login, $password, $firstName, $lastName);
+			$stmt->execute();
+			$id = $stmt->insert_id;
+			
+			if($id > 0)
+			{
+				returnWithInfo($id);
+			}
+			else
+			{
+				returnWithError("Registration failed");
+			}
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $id )
+	{
+		$retValue = '{"id":' . $id . ',"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
